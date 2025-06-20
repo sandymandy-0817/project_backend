65c7dc20a1528b99b3259c6338b7f87ba502228a
sandymandy-0817 <ksk3570@gmail.com>
2025-06-20 17:55:27 +0900

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SECRET_KEY = 'DiGong';
 const uploads = multer({ dest: 'uploads/' }); 
 
 connection = mysql.createConnection({
-  host:'database',
+  host:'svc.sel4.cloudtype.app',
   user:'root',
   password:'1234',
   database:'kdt'
